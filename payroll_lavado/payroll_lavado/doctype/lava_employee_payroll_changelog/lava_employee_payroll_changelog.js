151eb8bc8d5f16b6dfaca46a20350fca4942a1a1
Zynap <zobalhlooba@gmail.com>
2022-11-06 08:48:33 +0200

    Add doctypes & workspace

---
+++ payroll_lavado/payroll_lavado/doctype/lava_employee_payroll_changelog/lava_employee_payroll_changelog.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Lavaloon and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Lava Employee Payroll Changelog', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ payroll_lavado/payroll_lavado/doctype/lava_penalty_policy/test_lava_penalty_policy.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, Lavaloon and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestLavaPenaltyPolicy(unittest.TestCase):
+	pass
